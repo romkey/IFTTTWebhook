273c4d0b25cfffc7e5dd317a82b43f5eb9aae063
John Romkey <romkey@romkey.com>
2018-03-29 22:27:11 -0700

    got rid of extra version from library.json
    ESP32 support

--- src/IFTTTWebhook.cpp
+++ src/IFTTTWebhook.cpp
@@ -3,8 +3,16 @@
    Created by John Romkey - https://romkey.com/
    March 24, 2018
  */
+
+#ifndef ESP32
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#endif
+
+#ifdef ESP32
+#include <WiFi.h>
+#include <HTTPClient.h>
+#endif
 
 #include "IFTTTWebhook.h"
 
@@ -28,6 +36,38 @@ int IFTTTWebhook::trigger(const char* value1) {
 int IFTTTWebhook::trigger(const char* value1, const char* value2) {
   return IFTTTWebhook::trigger(value1, value2, NULL);
 }
+
+#ifdef ESP32
+const char* _ifttt_root_certificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx" \
+"EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT" \
+"EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp" \
+"ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMxMDUwMzA3" \
+"MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH" \
+"EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UE" \
+"CxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQD" \
+"EypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwggEi" \
+"MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYuswZLiBCGzD" \
+"BNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz6ojcnqOv" \
+"K/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am+GZHY23e" \
+"cSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1gO7GyQ5HY" \
+"pDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQWOlDxSq7n" \
+"eTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB0lL7AgMB" \
+"AAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjAdBgNV" \
+"HQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqFBxBnKLbv" \
+"9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8v" \
+"b2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDovL2NybC5n" \
+"b2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0gADAzMDEG" \
+"CCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkv" \
+"MA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyIBslQj6Zz" \
+"91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwlTxFWMMS2" \
+"RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKocyQetawi" \
+"DsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1KrKQ0U11" \
+"GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkKrqeKM+2x" \
+"LXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDAB" \
+"-----END CERTIFICATE-----\n";
+#endif
 
 int IFTTTWebhook::trigger(const char* value1, const char* value2, const char* value3) {
   HTTPClient http;
@@ -76,8 +116,13 @@ int IFTTTWebhook::trigger(const char* value1, const char* value2, const char* va
   Serial.println(ifttt_url);
 #endif
   
+#ifdef ESP32
+  // certificate: openssl s_client -showcerts -connect maker.ifttt.com:443 < /dev/null
+  http.begin(ifttt_url, _ifttt_root_certificate);
+#else
   // fingerprint: openssl s_client -connect maker.ifttt.com:443  < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout | cut -d'=' -f2
   http.begin(ifttt_url, _ifttt_fingerprint);
+#endif
   int httpCode = http.GET();
 
 #ifdef IFTTT_WEBHOOK_DEBUG  
